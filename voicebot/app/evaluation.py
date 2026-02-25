2a8669c7b39fda904796564513cd52ad738ee683
RODDA VAMSI KRISHNA REDDY <rvamsikrishnareddy333@gmail.com>
2026-02-25 22:30:35 +0530

    Update evaluation.py

--- voicebot/app/evaluation.py
+++ voicebot/app/evaluation.py
@@ -1,1 +1,10 @@
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+print(classification_report(y_true, y_pred))
+
+cm = confusion_matrix(y_true, y_pred)
+sns.heatmap(cm, annot=True)
+plt.show()
 
